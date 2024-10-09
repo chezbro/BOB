e507a8f24cb4c3ff7d5ff0b0f7c778b617235037
chezbro <ericchesbrough@gmail.com>
2024-10-09 15:55:40 -0700

    back to top

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -53,7 +53,14 @@ const Navbar = () => {
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
-          href={"/"}
+          href="#"
+          onClick={(e) => {
+            e.preventDefault();
+            window.scrollTo({
+              top: 0,
+              behavior: "smooth",
+            });
+          }}
           className="text-2xl md:text-3xl text-white font-semibold"
         >
           {shuffledText}
